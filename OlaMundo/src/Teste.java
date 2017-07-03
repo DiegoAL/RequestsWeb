7953d001566ae3e590eaa175eef324c559a921dd
DiegoAlv <diego.alves785@gmail.com>
2017-07-03 17:46:18 -0300

    Upload Projeto

---
+++ OlaMundo/src/Teste.java
@@ -0,0 +1,13 @@
+public class Teste {
+	public static void main(String[] args) {
+		System.out.println("ola");
+		conta c1 = new conta();
+		c1.saldo = 1;
+		System.out.println(c1.saldo);
+	}
+}
+
+class conta {
+	public int saldo;
+
+}
